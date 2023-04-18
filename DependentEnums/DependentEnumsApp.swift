dd55bb813c08115fa9e431db6bf81b956e22a5f7
reknoZ <david@zicount.com>
2023-04-18 15:02:04 +0200

    Initial Commit

---
+++ DependentEnums/DependentEnumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DependentEnumsApp.swift
+//  DependentEnums
+//
+//  Created by David on 18/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct DependentEnumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
